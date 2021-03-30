24448ae6d5fa9e5990fc99d3e70393de4e59a89d
hugomacielads <hugomacielcesar@gmail.com>
2021-03-29 22:38:18 -0300

    Correção fibonacci

--- Target sistemas/sequencia-fibonacci.js
+++ Target sistemas/sequencia-fibonacci.js
@@ -5,22 +5,12 @@
         /* Chamadas recursivas a função fibo() para sempre somar as duas últimas posições da sequência fibonacci*/ 
     }
 
-    // Posição 3 na seguência de Fibonacci: 1 + 2 = 3
-    console.log(fibo(3))
-
-    // Posição 5 na seguência de Fibonacci: 5 + 3 = 8
-    console.log(fibo(5))
-
-    // Posição 7 na seguência de Fibonacci: 13 + 8 = 21 
-    console.log(fibo(7))
-
     let fibonacci = []
 
-    fibonacci.push(fibo(1))
-    fibonacci.push(fibo(2))
-    fibonacci.push(fibo(3))
-    fibonacci.push(fibo(4))
-    fibonacci.push(fibo(5))
-    fibonacci.push(fibo(6))
+    for (let i = 1; i <= 15; i++) {
+        fibonacci.push(fibo(i))
+    }
 
     console.log(fibonacci)
+
+    
